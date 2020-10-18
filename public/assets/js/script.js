e6f8f3b65ae6851513aa04807352f26ae1d9794b
ccrum292 <ccrum292@gmail.com>
2020-10-17 20:32:01 -0400

    added new gifs for ldh

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,23 +1,23 @@
-console.log("workin");
+console.log('workin');
 
-$(document).ready(function(){
-  console.log(`loding`)
-  $('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true
-  });
-  $('.tabs').tabs();
-  setButtonColHeight();
-  $('.modal').modal();
-  $('input#input_text, textarea#textarea2').characterCounter();
-  getComments();
-  $('.scrollspy').scrollSpy({
-    scrollOffset: 0
-  });
-  $('.tooltipped').tooltip();
-  // $(".indicator-item").css('background-color','blue')
-  // $(".indicator-item.active").css('background-color','black')
-  calebCrumAnime();
+$(document).ready(function() {
+	console.log(`loding`);
+	$('.carousel.carousel-slider').carousel({
+		fullWidth: true,
+		indicators: true
+	});
+	$('.tabs').tabs();
+	setButtonColHeight();
+	$('.modal').modal();
+	$('input#input_text, textarea#textarea2').characterCounter();
+	getComments();
+	$('.scrollspy').scrollSpy({
+		scrollOffset: 0
+	});
+	$('.tooltipped').tooltip();
+	// $(".indicator-item").css('background-color','blue')
+	// $(".indicator-item.active").css('background-color','black')
+	calebCrumAnime();
 });
 
 // use later when themes have been added
@@ -29,29 +29,36 @@ $(document).ready(function(){
 
 // carousel arrows
 $('#prev1').click(function() {
-  $('.carousel.first-project-carousel').carousel('prev');
+	$('.carousel.first-project-carousel').carousel('prev');
 });
 
 $('#next1').click(function() {
-  $('.carousel.first-project-carousel').carousel('next');
+	$('.carousel.first-project-carousel').carousel('next');
 });
 
 $('#prev2').click(function() {
-  $('.carousel.second-project-carousel').carousel('prev');
+	$('.carousel.second-project-carousel').carousel('prev');
 });
 
 $('#next2').click(function() {
-  $('.carousel.second-project-carousel').carousel('next');
+	$('.carousel.second-project-carousel').carousel('next');
 });
 
 $('#prev3').click(function() {
-  $('.carousel.third-project-carousel').carousel('prev');
+	$('.carousel.third-project-carousel').carousel('prev');
 });
 
 $('#next3').click(function() {
-  $('.carousel.third-project-carousel').carousel('next');
+	$('.carousel.third-project-carousel').carousel('next');
 });
 
+$('#prev4').click(function() {
+	$('.carousel.fourth-project-carousel').carousel('prev');
+});
+
+$('#next4').click(function() {
+	$('.carousel.fourth-project-carousel').carousel('next');
+});
 
 // anime.js
 // not being used yet
@@ -65,97 +72,94 @@ $('#next3').click(function() {
 //   loop: true
 // });
 
-
 const setButtonColHeight = () => {
-  let pictureColHeight = $(".img-col")[0].scrollHeight
-  $(".button-col").css("height", pictureColHeight);
-}
+	let pictureColHeight = $('.img-col')[0].scrollHeight;
+	$('.button-col').css('height', pictureColHeight);
+};
 
+const copyToClipboard = (event) => {
+	console.log('button clicked');
+	console.log(event);
+	console.log($(event).text());
+	// event.preventDefault();
+	var $temp = $('<input>');
+	$('body').append($temp);
+	$temp.val($(event).text()).select();
+	console.log($temp.val($(event).text()).select());
+	document.execCommand('copy');
+	$temp.remove();
+	$('.modal-content').append(`<p>Success!</p>`);
+};
 
-const copyToClipboard = event => {
-  console.log("button clicked")
-  console.log(event);
-  console.log($(event).text())
-  // event.preventDefault();
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(event).text()).select();
-  console.log($temp.val($(event).text()).select())
-  document.execCommand("copy");
-  $temp.remove();
-  $(".modal-content").append(`<p>Success!</p>`)
-}
-
-$(".copy-button").click(() => copyToClipboard("#email-address"))
-
+$('.copy-button').click(() => copyToClipboard('#email-address'));
 
 // API Calls
 
 const postComments = (name, text) => {
-  $.ajax("/api/comments", {
-    type: "POST",
-    data: {
-      name: name.val().trim(),
-      text: text.val().trim()
-    }
-  }).then(() => location.reload())
-}
+	$.ajax('/api/comments', {
+		type: 'POST',
+		data: {
+			name: name.val().trim(),
+			text: text.val().trim()
+		}
+	}).then(() => location.reload());
+};
 
 const getComments = () => {
-  $.ajax("/api/comments", {
-    type: "GET"
-  }).then(data => {
-    const commentsDiv = $(`
+	$.ajax('/api/comments', {
+		type: 'GET'
+	}).then((data) => {
+		const commentsDiv = $(`
     <div style="margin-bottom: 50px" id="comments-div" class="row"></div>
     `);
-    $("#main-container-body").append(commentsDiv);
-    let cssStyleHeading = "alter-heading-4";
-    let cssStyleText = "alter-text-4";
-    data.forEach(val => {
-      // it else statement used to change color of heading and text for each entry
-      if(cssStyleHeading === "alter-heading-1"){
-        cssStyleHeading = "alter-heading-2";
-        cssStyleText = "alter-text-2";
-      }else if(cssStyleHeading === "alter-heading-2"){
-        cssStyleHeading = "alter-heading-3";
-        cssStyleText = "alter-text-3";
-      }else if(cssStyleHeading === "alter-heading-3"){
-        cssStyleHeading = "alter-heading-4";
-        cssStyleText = "alter-text-4";
-      }else if(cssStyleHeading === "alter-heading-4"){
-        cssStyleHeading = "alter-heading-1";
-        cssStyleText = "alter-text-1";
-      }
-      $("#comments-div").prepend(`
+		$('#main-container-body').append(commentsDiv);
+		let cssStyleHeading = 'alter-heading-4';
+		let cssStyleText = 'alter-text-4';
+		data.forEach((val) => {
+			// it else statement used to change color of heading and text for each entry
+			if (cssStyleHeading === 'alter-heading-1') {
+				cssStyleHeading = 'alter-heading-2';
+				cssStyleText = 'alter-text-2';
+			} else if (cssStyleHeading === 'alter-heading-2') {
+				cssStyleHeading = 'alter-heading-3';
+				cssStyleText = 'alter-text-3';
+			} else if (cssStyleHeading === 'alter-heading-3') {
+				cssStyleHeading = 'alter-heading-4';
+				cssStyleText = 'alter-text-4';
+			} else if (cssStyleHeading === 'alter-heading-4') {
+				cssStyleHeading = 'alter-heading-1';
+				cssStyleText = 'alter-text-1';
+			}
+			$('#comments-div').prepend(`
       <div class="col s12 m12 l12 xl12">
         <h5 class="right-align ${cssStyleHeading}">${val.name}</h5>
         <p class="right-align ${cssStyleText}">${val.text}</p>
       </div>
-      `)
-    })
-  })
-}
+      `);
+		});
+	});
+};
 
 const deleteComments = (id) => {
-  $.ajax("", {
-    type: "DELETE",
-    data: {
-      id: id
-    }
-  })
-}
+	$.ajax('', {
+		type: 'DELETE',
+		data: {
+			id: id
+		}
+	});
+};
 
 // Submit Comment Button
 
-$("#submitComment").on("click", event => {
-  event.preventDefault();
-  postComments($("#input_text"), $("#textarea2"));
-})
+$('#submitComment').on('click', (event) => {
+	event.preventDefault();
+	postComments($('#input_text'), $('#textarea2'));
+});
 
 // styling
 
-$(".indicator-item").css('background-color','blue')
-$(".indicator-item.active").css('background-color','black')
+$('.indicator-item').css('background-color', 'blue');
+$('.indicator-item.active').css('background-color', 'black');
 
 // TRYING to fix weird bug with nav tabs
 // $(".tab").on("click", event => {
